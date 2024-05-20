1106456be8973c9407093754940f2612b79e71ef
Butawantemi <japhetbuta@gmail.com>
2024-05-20 18:23:31 +0300

    basic authentication

---
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -0,0 +1,42 @@
+
+@app.before_request
+def before_request():
+    """before request method"""
+    if auth is None:
+        pass
+    else:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+        ]
+
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401, "Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, "Forbidden")
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized Error"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden Error"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
