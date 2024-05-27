53284e7d74bd9808c529c5a90bed75d1aabf1156
Butawantemi <japhetbuta@gmail.com>
2024-05-27 09:15:13 +0300

    start basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic Flask app, Register user, Log in, Log out, User profile,
+    Get reset passwords token, Update password end-point """
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def welcome():
+    """Basic Flask app, return a JSON"""
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
